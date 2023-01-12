3f25f2d4b87c0d09d92da9a02b71592a8a322a04
@Pradip Khamkar <119717646+PradipKhamkar@users.noreply.github.com>
2023-01-12 04:46:30 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,12 @@ app.use(express.json());
 //Use Express File Upload
 app.use(expressFileUpload());
 
-
+//Config Cloudniary
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET_KEY,
+});
 
 app.listen(process.env.PORT, "localHost", () => {
   console.log(`Server Running At http://localhost:${process.env.PORT}`);
